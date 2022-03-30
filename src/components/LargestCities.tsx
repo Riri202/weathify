d566e9cac544130086a7a01865e3c6089ec270e4
Rita-Maria Oladokun <Rita-Maria@Rita-Marias-MacBook-Pro.local>
2022-03-30 16:01:30 +0100

    style: fix space between

--- src/components/LargestCities.tsx
+++ src/components/LargestCities.tsx
@@ -57,17 +57,17 @@ export const  LargestCities: React.FC<LargestCitiesProps> = ({setCity, tempData,
         <div className='flex flex-col md:flex-row md:flex-wrap w-[80vw] md:w-auto'>
             {cities && cities.map((city, key) => {
                 return (
-                <div key={key} className='flex mb-3 pt-4 pb-4 pl-3 pr-3 mr-3 flex-row justify-between items-start space-x-5 w-auto rounded-sm cursor-pointer bg-stone-50 opacity-60 brightness-200 hover:opacity-100 active:opacity-100 focus:opacity-100 transition-all ease-in-out duration-150'>
+                <div key={key} className='flex mb-3 pt-4 pb-4 pl-3 pr-3 mr-3 flex-row justify-between items-start space-x-12 w-auto rounded-sm cursor-pointer bg-stone-50 opacity-60 brightness-200 hover:opacity-100 active:opacity-100 focus:opacity-100 transition-all ease-in-out duration-150'>
                     <div className=''>
                         <Link to='/weatherDetails'>
                         <button className='text-xl text-stone-900  border-b-2 border-b-stone-900 font-bold uppercase' onClick={() => getWeather(city)}>
                         {city}
                         </button>
                         </Link>
-                        <button onClick={() => showCityTemp(city)} className='text-red-900 ml-4'><FaAngleDown/></button>
+                        <button onClick={() => showCityTemp(city)} className='text-stone-900 ml-4'><FaAngleDown/></button>
                         {city === cityName ?  <p className='text-red-900 text-xl md:text-3xl mt-3'>{temp} ºC</p> : null}
                     </div>
-                        <button onClick={() => deleteCities(city)}><FaTimes className='text-red-500 font-bold'/></button>
+                        <button onClick={() => deleteCities(city)}><FaTimes className='text-red-600 font-bold'/></button>
                 </div>
                 )
             })}
