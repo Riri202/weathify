702e909c42dd6b43f91ff93f4a784243dd3f5126
Rita-Maria Oladokun <Rita-Maria@Rita-Marias-MacBook-Pro.local>
2022-04-04 11:39:47 +0100

    ft-display weather details on btn click

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,23 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { InputProps } from "../types/types";
-import FavsList  from './FavsList';
+import axios from "axios";
+import { Temp, Weather, tempDefault, weatherDefault } from "../types/types";
+import WeatherDetails from '../pages/WeatherDetails';
 
 
-export  const Input: React.FC<InputProps> = ({setCity, city}) => {
-  const [favs, setFavs] = useState(
-    [
-        'Berlin', // default favorite list that shows on load
-        'London',
-        'Tokyo'
-    ]
-)
-const [newFav, setNewFav] = useState<string>('')
+
+
+
+
+export  const Input: React.FC<InputProps> = () => {
+
+const [tempData, setTempData] = useState<Temp>(tempDefault);
+const [weatherData, setWeatherData] = useState<Weather>(weatherDefault);
+const [cityName, setCityName] = useState<string>('')
+const [temp, setTemp] = useState<number>(0)
+const [city, setCity] = useState<string>("");
+const [loading, setLoading] = useState(false)
+const navigate = useNavigate()
+ 
   
     const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setCity(event.target.value)
-        setNewFav(event.target.value)
-   
+      setCity(event.target.value)    
     }
     const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
@@ -25,39 +31,39 @@ const [newFav, setNewFav] = useState<string>('')
 
     }
     // search for city weather details
-    const searchHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-         setCity(e.currentTarget.value) 
+    const searchHandler = async () => {
+      if (city !== undefined) {
+        let response = await axios(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=e002b44efb0f426f0c67533d6ace14bd`
+        );
+        let result = await response.data;
+        setCityName(result.name)
+        setTemp(result.main.temp)
+        setTempData(result.main);
+        setWeatherData(result.weather[0]);
+        setLoading(true)
+        console.log(result);
+      }
+      // navigate('/weatherDetails')
     }
-// add new cities to favourite list
-const addToFavs = (e: React.MouseEvent<HTMLButtonElement>) => {
-  setFavs([...favs, newFav ].sort() )  // return the favorite list sorted in alphabetical order as new cities are added
+    
   
-}
-// delete cities from the favorites list
-const deleteHandler = (location: string) => {
-  setFavs(favs.filter(fav => fav !== location)) 
-}
 
     
    
 
   return (
     <div className='flex flex-col space-y-10 w-[90vw] md:w-[80vw]'>
-    <div className=' mb-10 p-2 md:p-0'>
-      <p className='text-base md:text-2xl mb-3 font-serif tracking-widest'>Favourites List</p>
-      <FavsList favs={favs} deleteHandler={deleteHandler}/>
-    </div>
+
     <div className='flex p-2 md:p-0 flex-col'>
       <form onSubmit={submitHandler} className='flex flex-col space-y-10'>
-        <input placeholder='Enter a city name' type='text' value={city} onChange={changeHandler} className=' focus-within:text-stone-200 outline-none bg-transparent border-0 border-b-2 border-b-stone-50 text-sm md:text-lg pl-3 pt-3'/>
+        <input placeholder='Enter a city name' type='text' value={city || ''} onChange={changeHandler} className=' focus-within:text-stone-200 outline-none bg-transparent border-0 border-b-2 border-b-stone-50 text-sm md:text-lg pl-3 pt-3'/>
         <div className='flex space-x-10'>
-          <Link to='/weatherDetails'>
-        <button className='p-2 uppercase text-stone-900 bg-stone-50 hover:text-[#fff] hover:bg-transparent hover:ring-1 hover:ring-stone-50 transition-all duration-200 ease-out rounded-sm text-sm md:text-xl' onClick={searchHandler}>get weather details</button>
-          </Link>
-        <button className='ring-1 ring-stone-50 p-2 uppercase text-sm md:text-xl' onClick={addToFavs}>add to favorites</button>
+        <button onClick={searchHandler} className='p-2 uppercase text-stone-900 bg-stone-50 hover:text-[#fff] hover:bg-transparent hover:ring-1 hover:ring-stone-50 transition-all duration-200 ease-out rounded-sm text-sm md:text-xl' >get weather details</button>
         </div>
       </form>
     </div>
+    {loading ? <WeatherDetails city={city}  cityName={cityName} tempData={tempData} weatherData={weatherData}/> : null}
     </div>
   );
 }
