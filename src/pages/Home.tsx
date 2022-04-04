30659f9cea10b80de83a60678b71b9b8d8973f60
Rita-Maria Oladokun <Rita-Maria@Rita-Marias-MacBook-Pro.local>
2022-04-04 11:41:22 +0100

    style: fix page height

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,17 +7,15 @@ import { HomeProps } from "../types/types";
 
 export const Home: React.FC<HomeProps> = ({setCity, city, tempData, cityName, setTemp, temp}) => {
   return (
-    <div className='md:p-10 backdrop-blur-sm h-[400vh] md:h-[150vh]'>
-      <div className='flex flex-col space-y-20 items-center w-auto'>
+    <div className='md:p-10 backdrop-blur-sm h-[500vh] md:h-[450vh]'>
+      <div className='flex flex-col space-y-20 items-center'>
 
       <div className='flex flex-row w-auto mt-10 md:mb-0'>
      <Input setCity={setCity} city={city}/>
       </div>
 
-      <div className=' w-auto  p-8 md:pt-10 flex flex-row justify-center '>
-        <div className=''>
+      <div className='p-8 md:pt-10 flex flex-row'>
      <LargestCities tempData={tempData} cityName={cityName} setTemp={setTemp} temp={temp} setCity={setCity}/>
-        </div>
       </div>
 
       </div>     
