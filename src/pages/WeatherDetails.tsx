77b04eaf21a30eeac92294aaa2269f15583cc9e9
Rita-Maria Oladokun <Rita-Maria@Rita-Marias-MacBook-Pro.local>
2022-04-04 11:40:39 +0100

    style: fix page height

--- src/pages/WeatherDetails.tsx
+++ src/pages/WeatherDetails.tsx
@@ -7,22 +7,18 @@ import { WeatherDetailsProps } from "../types/types";
 
 
 export const WeatherDetails: React.FC<WeatherDetailsProps> = ({tempData, weatherData, city, cityName}) => {
-  const [notes, setNotes] = useState<string[]>(
-    [
-       'Your Note',
-       'New Note'    
-  ]
-  )
+  const [notes, setNotes] = useState<string[]>([])
    // eslint-disable-next-line @typescript-eslint/no-unused-vars
    const [newNote, setNewNote] = useState<string>('')
 
   
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setNewNote(event.target.value)
   }
   // add new notes
-  const addNewNote = (e: React.MouseEvent<HTMLButtonElement>) => {
-   setNotes([...notes, newNote])
+  const addNewNote = () => {
+    newNote.split('').length > 0 ?
+      setNotes([...notes, newNote]) : alert('Oops! You cannot add blank notes. Please type in the text area to add to a new note')  
   }
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -36,13 +32,13 @@ const handleDelete = (message: string) => {
 
   
   return (
-      <div className='p-5 md:p-10 flex flex-col-reverse md:flex-row md:justify-between md:h-[130vh] backdrop-blur-sm'> 
+      <div className='p-5 md:p-10 flex flex-col-reverse md:flex-row md:justify-between  backdrop-blur-sm'> 
       <div className='w-auto md:w-1/2'>
       <div className=' md:p-20 w-auto'>
           {/* <h1>{tempData.temp}</h1> */}
           <form onSubmit={handleSubmit} className='flex flex-col space-y-10 pt-10 pb-10 md:pb-0 md:pt-0'>
-            <input type='text' placeholder='Enter Notes' onChange={handleChange} className=' focus-within:text-stone-200 outline-none bg-transparent border-0 border-b-2 border-b-stone-50 text-sm md:text-lg pl-3 pt-3'>
-            </input>
+            <textarea  placeholder='Enter Notes' onChange={handleChange} className=' focus-within:text-stone-200 outline-none bg-transparent border-0 border-b-2 border-b-stone-50 text-sm md:text-lg pl-3 pt-3'>
+            </textarea>
             <button onClick={addNewNote} className='p-2 uppercase text-stone-900 bg-stone-50 hover:text-[#fff] hover:bg-transparent hover:ring-1 hover:ring-stone-50 transition-all duration-200 ease-out  rounded-sm text-sm md:text-xl'>Add New Note</button>
           </form>
       </div>
@@ -71,7 +67,7 @@ const handleDelete = (message: string) => {
             <p className='text-3xl md:text-5xl'>deg Celsius</p>
           </div>
           <div className='flex flex-col space-y-2'>
-            <img src={`http://openweathermap.org/img/wn/${weatherData.icon}.png`} alt="weather-desc-icon" className='w-14 h-14 md:w-16 md:h-16 bg-stone-300 rounded-lg'/>
+            <img src={weatherData.icon !== undefined ? `http://openweathermap.org/img/wn/${weatherData.icon}.png` : undefined} alt="weather-desc-icon" className='w-14 h-14 md:w-16 md:h-16 bg-stone-300 rounded-lg'/>
             <p className='text-sm md:text-base uppercase'>{weatherData.description}</p>
           </div>
         </div>
