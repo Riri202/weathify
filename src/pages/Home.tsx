eb36fd9f1d291a3b1120333cf5336cd725a7170f
Rita-Maria Oladokun <Rita-Maria@Rita-Marias-MacBook-Pro.local>
2022-04-05 02:31:55 +0100

    style: fix page height

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ import { HomeProps } from "../types/types";
 
 export const Home: React.FC<HomeProps> = ({setCity, city, tempData, cityName, setTemp, temp}) => {
   return (
-    <div className='md:p-10 backdrop-blur-sm h-[500vh] md:h-[450vh]'>
+    <div className='md:p-10 backdrop-blur-sm '>
       <div className='flex flex-col space-y-20 items-center'>
 
       <div className='flex flex-row w-auto mt-10 md:mb-0'>
