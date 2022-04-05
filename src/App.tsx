0c5c7d50e3d7263dc9cab58674a6c202c0f01588
Rita-Maria Oladokun <Rita-Maria@Rita-Marias-MacBook-Pro.local>
2022-04-05 02:32:44 +0100

    change API key

--- src/App.tsx
+++ src/App.tsx
@@ -23,14 +23,14 @@ import Navbar from './components/Navbar';
     const fetchData = async () => {
       if (city !== '') {
         let response = await axios(
-          `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=e002b44efb0f426f0c67533d6ace14bd`
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=a93119d28619d48fb6e748ef894f2870`
         );
         let result = await response.data;
         setCityName(result.name)
         setTemp(result.main.temp)
         setTempData(result.main);
         setWeatherData(result.weather[0]);
-        console.log(result);
+        //console.log(result);
       }
     };
     useEffect(() => {fetchData();
@@ -39,13 +39,14 @@ import Navbar from './components/Navbar';
   
 
   return (
-    <div className="text-[#fff] p-0 m-0 overflow-hidden" style={{ backgroundImage: `url(${background})`, objectFit:'cover', backgroundRepeat: 'repeat'}}>
+    <div className="text-[#fff] p-0 m-0 overflow-hidden " style={{ backgroundImage: `url(${background})`, objectFit:'cover', backgroundRepeat: 'repeat'}}>
     <Navbar/>
       <Router>
      <Routes>
        <Route path='/' element={<Home setCity={setCity} temp={temp} setTemp={setTemp} city={city} cityName={cityName} tempData={tempData}/>}/>
        <Route path='/weatherDetails' element={<WeatherDetails city={city}  cityName={cityName} tempData={tempData} weatherData={weatherData} />}/>     </Routes>
       </Router>
+      <div className='p-20 flex justify-center items-center backdrop-blur-sm'> <p>Powered by  <a href='https://openweathermap.org/api' className='uppercase text-red-700'>Open Weather API</a></p></div>
     </div>
   );
 };
